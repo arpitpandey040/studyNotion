53b25c01f2e8f4b807f057918605feae4f0ba63f
Arpit <ampandey58@gmail.com>
2024-01-22 05:17:06 +0530

    intial commit

---
+++ src/data/footer-links.js
@@ -0,0 +1,55 @@
+export const FooterLink2 = [
+  {
+    title: "Subjects",
+    links: [
+      { title: "Al", link: "/" },
+      { title: "Cloud Computing", link: "/" },
+      { title: "Code Foundations", link: "/" },
+      { title: "Computer Science", link: "/" },
+      { title: "Cybersecurity", link: "/" },
+      { title: "Data Analytics", link: "/" },
+      { title: "Data Science", link: "/" },
+      { title: "Data Visualization", link: "/" },
+      { title: "Developer Tools", link: "/" },
+      { title: "DevOps", link: "/" },
+      { title: "Game Development", link: "/" },
+      { title: "IT", link: "/" },
+      { title: "Machine Learning", link: "/" },
+      { title: "Math", link: "/" },
+      { title: "Mobile Development", link: "/" },
+      { title: "Web Design", link: "/" },
+      { title: "Web Development", link: "/" },
+    ],
+  },
+  {
+    title: "Languages",
+    links: [
+      { title: "Bash", link: "/" },
+      { title: "C++", link: "/" },
+      { title: "C#", link: "/" },
+      { title: "Go", link: "/" },
+      { title: "HTML & CSS", link: "/" },
+      { title: "Java", link: "/" },
+      { title: "JavaScript", link: "/" },
+      { title: "Kotlin", link: "/" },
+      { title: "PHP", link: "/" },
+      { title: "Python", link: "/" },
+      { title: "R", link: "/" },
+      { title: "Ruby", link: "/" },
+      { title: "SQL", link: "/" },
+      { title: "Swift", link: "/" },
+    ],
+  },
+  {
+    title: "Career building",
+    links: [
+        {title: "Career paths", link: "/"},
+        {title: "Career services", link: "/"},
+        {title: "Interview prep", link: "/"},
+        {title: "Professional certification", link: "/"},
+        {title: "-", link: "/"},
+        {title: "Full Catalog", link: "/"},
+        {title: "Beta Content", link: "/"}
+    ]
+  }
+];
